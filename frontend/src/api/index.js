567fef07c6f2564aa093839771f7b69185340586
Sanjana Naikar <sanjana.nkr28@gmail.com>
2026-05-24 00:49:13 +0530

    update api url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({
+  baseURL: "https://kanban-app-tdzd.onrender.com/api",
+});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
